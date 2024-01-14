6b03542b925c0f3abceef06312f6e16dbea3cebb
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2024-01-14 20:25:17 +0900

    Add files via upload

---
+++ image_classification.py
@@ -0,0 +1,19 @@
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+# ResNet 모델 로드
+model = ResNet50(weights='imagenet')
+
+# 입력 이미지 불러오기
+img_path = 'test_img.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+img_array = image.img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+img_array = preprocess_input(img_array)
+
+# 이미지 분류 예측
+predictions = model.predict(img_array)
+decoded_predictions = decode_predictions(predictions, top=3)[0]
+print(decoded_predictions)
